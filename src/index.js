91c23740c31d0f67b77473a165224c32e6ac4aab
Taha <taha.othman@outlook.com>
2023-03-28 18:05:48 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector('form')
+      form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      handleToDo(e.target.new_task_description.value)
+      form.reset()
+      
+    
+        
+  })
+
+})
+
+function handleToDo(text) {
+  let li = document.createElement('li')
+  let butn = document.createElement('button')
+  butn.addEventListener('click', deleteTask);
+  butn.textContent = "X"
+  li.textContent = `${text    }             `
+  li.appendChild(butn)
+  document.getElementById('tasks').append(li);
+
+
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
